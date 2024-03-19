fec35beace4bdf019bb1a3d003faad96e3b032a0
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-03-19 13:55:45 +0700

    Modules, Controller

---
+++ src/modules/products/product.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { ProductService } from "./product.service";
+
+@Controller('product')
+export class ProductController {
+
+    constructor(private readonly productService: ProductService) {
+
+    }
+
+    @Get()
+    getProducts(): string {
+        return this.productService.getProducts();
+    }
+
+    @Post()
+    createProduct() : string {
+        return this.productService.createProduct();
+    }
+
+    @Get('/:id')
+    detailProduct() : string {
+        return this.productService.detailProduct();
+    }
+
+    @Put('/:id')
+    updateProduct() : string {
+        return this.productService.updateProduct();
+    }
+
+    @Delete('/:id')
+    deleteProduct() : string {
+        return this.productService.deleteProduct();
+    }
+}
