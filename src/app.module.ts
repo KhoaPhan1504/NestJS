e054077a84fbaac4c857fc12cc8398fbfdcaeef7
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-03-19 15:32:26 +0700

    Connect MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/products/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'khoadev#',
+      database: 'nestjs_api',
+      entities: [],
+      synchronize: true,
+    }),
     ProductModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    
+  }
+}
